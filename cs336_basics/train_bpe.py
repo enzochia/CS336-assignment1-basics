4306411852806c0f51e9e0a9cdfc93990d4d536d
Chaoqun Jia <enzo.jia@gmail.com>
2025-07-26 23:17:37 -0400

    a working version of train_bpe.py. Passed tests.

--- cs336_basics/train_bpe.py
+++ cs336_basics/train_bpe.py
@@ -2,7 +2,7 @@ import os
 import regex as re
 import multiprocessing
 import heapq
-import tqdm
+from tqdm import tqdm
 from typing import List, Dict, Tuple, BinaryIO, Union
 from collections import Counter
 
@@ -10,6 +10,7 @@ PAT_STR_GPT2 = r"""'(?:[sdmt]|ll|ve|re)| ?\p{L}+| ?\p{N}+| ?[^\s\p{L}\p{N}]+|\s+
 ENDOFTEXT = "<|endoftext|>"
 SPECIAL_TOKENS = ["<|endoftext|>"]
 # SPECIAL_TOKENS = ["<|endoftext|>".encode('utf-8')]
+# CORPUS_FILE = "data/TinyStoriesV2-GPT4-debug.txt"
 CORPUS_FILE = "data/TinyStoriesV2-GPT4-valid.txt"
 
 def find_chunk_boundaries(
@@ -60,41 +61,36 @@ def find_chunk_boundaries(
     # Make sure all boundaries are unique, but might be fewer than desired_num_chunks
     return sorted(set(chunk_boundaries))
 
-def tokenize_chunk(text_chunk):
+
+def tokenize_chunk(text_chunk: str) -> Dict[str, int]:
     escaped_tokens = [re.escape(token) for token in SPECIAL_TOKENS]
     pattern_split = "|".join(escaped_tokens)
-    text_chunk = "".join(re.split(pattern_split, text_chunk))
-    return Counter(re.findall(PAT_STR_GPT2, text_chunk))
-
-# # a version using bytes rather than string
-# def tokenize_chunk(text_chunk):
-#     escaped_tokens = [re.escape(token) for token in SPECIAL_TOKENS]
-#     pattern_split = "|".encode('utf-8').join(escaped_tokens)
-#     text_chunk = "".encode('utf-8').join(re.split(pattern_split, text_chunk))
-#     return Counter(re.findall(PAT_STR_GPT2.encode('utf-8'), text_chunk))
+    text_chunks = re.split(pattern_split, text_chunk)
+    if text_chunks:
+        pretokenized_chunk = Counter(re.findall(PAT_STR_GPT2, text_chunks[0]))
+        for chunk in text_chunks[1:]:
+            pretokenized_chunk.update(Counter(re.findall(PAT_STR_GPT2, chunk)))
+    return pretokenized_chunk
 
 
 def pre_tokenize(input_path: str,
                  num_processes: int) -> Dict[Tuple[bytes], int]:
     with open(input_path, "rb") as f:
-        chunk_boundaries = find_chunk_boundaries(f, num_processes, ENDOFTEXT.encode('utf-8'))
-        chunks = []
+        chunk_boundaries: List[int] = find_chunk_boundaries(f, num_processes, ENDOFTEXT.encode('utf-8'))
+        chunks: List[str] = []
         f.seek(0)
         for idx_chunk in range(len(chunk_boundaries) - 1):
             idx_chunk_start = chunk_boundaries[idx_chunk]
             idx_chunk_end = chunk_boundaries[idx_chunk + 1]
             chunks.append(f.read(idx_chunk_end - idx_chunk_start).decode("utf-8", errors="ignore"))
-            # # corresponds to the bytes version of tokenize_chunk method
-            # chunks.append(f.read(idx_chunk_end - idx_chunk_start))
-
 
     with multiprocessing.Pool(processes=num_processes) as pool:
-        results = pool.map(tokenize_chunk, chunks)
+        results: List[Dict[str, int]] = pool.map(tokenize_chunk, chunks)
 
-    pretokenization_dict = results[0]
+    pretokenization_dict: Dict[str, int] = results[0]
     for chunk_counter in results[1:]:
         pretokenization_dict.update(chunk_counter)
-    pretoken_counter = {tuple(b for b in key.encode("utf-8")): val for key, val in pretokenization_dict.items()}
+    pretoken_counter: Dict[Tuple[bytes], int] = {tuple(bytes([b]) for b in key.encode("utf-8")): val for key, val in pretokenization_dict.items()}
     return pretoken_counter
 
 
@@ -113,79 +109,119 @@ def is_subtuple(subtuple: Tuple[bytes],
             idx += len(subtuple)
         else:
             idx += 1
-    return(sub_tuple_idx_list)
+    return sub_tuple_idx_list
 
 
 def _add_bytes_in_tuple(tuple_input: Tuple[bytes]) -> bytes:
     new_bytes: bytes = tuple_input[0]
     for b in tuple_input[1:]:
         new_bytes += b
-    return(new_bytes)
+    return new_bytes
     
+
+def _add_and_pop_for_collapse(fulltuple: Tuple[bytes],
+                              sub_tuple_idx_list: List[int],
+                              new_tuple: Tuple[bytes],
+                              merge_size: int = 2) -> Tuple[Dict[Tuple[bytes], int]]:
+    idx_pair_to_add_in_new_tuple: Set[int] = set(idx_pair_to_add
+                                                 for idx_collapse, idx_fulltuple in enumerate(sub_tuple_idx_list)
+                                                 for idx_pair_to_add in [idx_fulltuple - (idx_collapse * (merge_size - 1)) - 1,
+                                                                         idx_fulltuple - (idx_collapse * (merge_size - 1))])
+    idx_pair_to_add_in_new_tuple = set(idx_pair_to_add for idx_pair_to_add in idx_pair_to_add_in_new_tuple
+                                                       if (idx_pair_to_add >=0) and (idx_pair_to_add < (len(fulltuple) - len(sub_tuple_idx_list) * (merge_size - 1) - 1)))
+
+    idx_pair_to_pop_in_fulltuple: Set[int] = set(idx_pair_to_pop 
+                                                 for idx_collapse, idx_fulltuple in enumerate(sub_tuple_idx_list)
+                                                 for idx_pair_to_pop in [idx_fulltuple - 1, idx_fulltuple + merge_size - 1])
+    idx_pair_to_pop_in_fulltuple = set(idx_pair_to_pop for idx_pair_to_pop in idx_pair_to_pop_in_fulltuple
+                                                       if idx_pair_to_pop >= 0 and (idx_pair_to_pop < (len(fulltuple) - 1)))
+
+    pair_to_add_dict: Dict[Tuple[bytes], int] = {}
+    pair_to_pop_dict: Dict[Tuple[bytes], int] = {}
+
+    for idx in idx_pair_to_add_in_new_tuple:
+        pair_to_add_dict.setdefault(new_tuple[idx:(idx + merge_size)], 0)
+        pair_to_add_dict[new_tuple[idx:(idx + merge_size)]] += 1
+
+    for idx in idx_pair_to_pop_in_fulltuple:
+        pair_to_pop_dict.setdefault(fulltuple[idx:(idx + merge_size)], 0)
+        pair_to_pop_dict[fulltuple[idx:(idx + merge_size)]] += 1 
+    return(pair_to_add_dict, 
+           pair_to_pop_dict)
+
 
 def collapse_tuple(fulltuple: Tuple[bytes], 
                    sub_tuple_idx_list: List[int],
-                   sub_tuple_len: int = 2) -> Tuple[bytes]:
-    new_tuple = fulltuple[:sub_tuple_idx_list[0]]
-    for idx_subtuple in range(len(sub_tuple_idx_list)):
-        new_bytes = _add_bytes_in_tuple(fulltuple[sub_tuple_idx_list[idx_subtuple]:(sub_tuple_idx_list[idx_subtuple] + 2)])
-        new_tuple += tuple([new_bytes])
-        if idx_subtuple < (len(sub_tuple_idx_list) - 1):
-            new_tuple += fulltuple[(sub_tuple_idx_list[idx_subtuple] + 2):sub_tuple_idx_list[idx_subtuple + 1]]
-        else:
-            new_tuple += fulltuple[(sub_tuple_idx_list[idx_subtuple] + 2):]
-    return(new_tuple)
+                   collapsed_bytes: bytes,
+                   merge_size: int = 2) -> Tuple[Union[Dict[Tuple[bytes], int], Tuple[bytes]]]:
+    assert len(sub_tuple_idx_list) > 0 and sub_tuple_idx_list[0] >= 0 and sub_tuple_idx_list[-1] <= len(fulltuple) - merge_size
+    sub_tuple_idx_list = [-merge_size] + sub_tuple_idx_list
+    new_tuple: Tuple[bytes] = ()
+    for idx_subtuple in range(1, len(sub_tuple_idx_list)):
+        new_tuple += fulltuple[(sub_tuple_idx_list[idx_subtuple - 1] + merge_size):sub_tuple_idx_list[idx_subtuple]] + \
+                     tuple([collapsed_bytes])
+    new_tuple += fulltuple[(sub_tuple_idx_list[-1] + merge_size):]
+    pair_to_add_dict, pair_to_pop_dict = _add_and_pop_for_collapse(fulltuple, sub_tuple_idx_list[1:], new_tuple)
+    return(new_tuple, 
+           pair_to_add_dict, 
+           pair_to_pop_dict)
 
 
 def train_bpe(input_path: str,
               vocab_size: int,
               special_tokens: List[str]) -> Tuple[Union[Dict, List]]:
-    num_processes = os.cpu_count()
-    pretoken_counter = pre_tokenize(input_path, num_processes)
+    num_processes: int = os.cpu_count()
+    pretoken_counter: Dict[Tuple[bytes], int] = pre_tokenize(input_path, num_processes)
 
-    print("Most common pre-tokens:")
-    print([(bytes(x).decode("utf-8"), pretoken_counter[x]) for x in heapq.nlargest(10, pretoken_counter, key=lambda x: pretoken_counter[x])])
-    print([(x, pretoken_counter[x]) for x in heapq.nlargest(10, pretoken_counter, key=lambda x: pretoken_counter[x])])
-    print("Longest pre-tokens:")
-    print([bytes(x).decode("utf-8") for x in heapq.nlargest(10, pretoken_counter, key=lambda x: len(x))])
+    merges: List[Tuple[bytes]] = []
+    vocab: Dict[int, bytes] = {key: bytes([key])
+             for key in range(256)}
+    for idx, token in enumerate(special_tokens):
+        vocab[idx + 256] = token.encode("utf-8")
 
-    # merges = []
-    # vocab = {key: bytes([key])
-    #          for key in range(256)}
-    # for idx, token in enumerate(special_tokens):
-    #     vocab[idx + 256] = token.encode("utf-8")
-    
-    # pair_freq = {}
-    # for pretoken_tuple, freq in tqdm(pretoken_counter.items()):
-    #     for idx_pair in range(len(pretoken_tuple) - 1):
-    #         pair_tuple = pretoken_tuple[idx_pair:(idx_pair + 2)]
-    #         pair_freq.setdefault(pair_tuple, 0)
-    #         pair_freq[pair_tuple] += freq
+    pair_freq: Dict[Tuple[bytes], int] = {}
+    for pretoken_tuple, freq in tqdm(pretoken_counter.items()):
+        for idx_pair in range(len(pretoken_tuple) - 1):
+            pair_tuple = pretoken_tuple[idx_pair:(idx_pair + 2)]
+            pair_freq.setdefault(pair_tuple, 0)
+            pair_freq[pair_tuple] += freq
 
-    # counter_iter = 0
-    # while max(counter_iter, len(vocab)) < vocab_size:
-    #     counter_iter += 1
-    #     max_pair = max(pair_freq, key=lambda x:(pair_freq[x], x))
+    pbar = tqdm(total=vocab_size - len(vocab))
+    while len(vocab) < vocab_size:
+        if not pair_freq:
+            break
+        max_pair = max(pair_freq, key=lambda x:(pair_freq[x], x))
+        new_bytes_token = _add_bytes_in_tuple(max_pair)
+        pretoken_pair_to_replace = []
+        for pretoken, pretoken_freq in pretoken_counter.items():
+            sub_tuple_idx_list = is_subtuple(max_pair, pretoken)
+            if sub_tuple_idx_list:
+                pair_freq[max_pair] -= len(sub_tuple_idx_list) * pretoken_freq
+                if pair_freq[max_pair] == 0:
+                    pair_freq.pop(max_pair)
+                new_pretoken, \
+                pair_to_add_dict, \
+                pair_to_pop_dict = collapse_tuple(pretoken, sub_tuple_idx_list, new_bytes_token)
+                pretoken_pair_to_replace.append([pretoken, new_pretoken])
+                for pair, freq in pair_to_add_dict.items():
+                    pair_freq.setdefault(pair, 0)
+                    pair_freq[pair] += freq * pretoken_freq
+                for pair, freq in pair_to_pop_dict.items():
+                    pair_freq[pair] -= freq * pretoken_freq
+                    if pair_freq[pair] == 0:
+                        pair_freq.pop(pair)
+        for pretoken_old, pretoken_new in pretoken_pair_to_replace:
+            # setdefault here? not doing it because pretoken_new is supposed to be unique
+            pretoken_counter[pretoken_new] = pretoken_counter[pretoken_old]
+            pretoken_counter.pop(pretoken_old)
+        merges.append(max_pair)
+        vocab[len(vocab)] = new_bytes_token
+        pbar.update(1)
+    pbar.close()
+    return vocab, merges
 
-            
-
-
-            
-
-
-
-    
-
-
-
-
-    # return vocab, merges
 
 if __name__ == "__main__":
     train_bpe(CORPUS_FILE,
               vocab_size=10000,
               special_tokens=SPECIAL_TOKENS)
-
-
-
-
